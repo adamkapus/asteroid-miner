d3597b99ea5148a7d02480767fc0b91909753b03
hoppare <hoppare@sch.bme.hu>
2021-03-17 13:58:16 +0100

    pls

--- szkeleton/src/szkeleton/Main.java
+++ szkeleton/src/szkeleton/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
 		// TODO Auto-generated method stub
 		//KA
-<<<<<<< HEAD
+
 
 		//Zsolt merge conflict pls
 
-=======
+
 		// Botond
 		//Zsolt
 		//Csinszka
 		//Márk
->>>>>>> f203cced79425260b3a61805e78bf0fd7755c635
+
 	}
 
 }
