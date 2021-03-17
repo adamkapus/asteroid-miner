cb045e26669d7154a1c08489de79cd2ffb31a9ee
hoppare <hoppare@sch.bme.hu>
2021-03-17 15:51:22 +0100

    Main osztály hozzadva, Szkeleton osztaly vaza elkeszitve - szamok beolvasasa

---
+++ szkeleton/src/szkeleton/Main.java
@@ -0,0 +1,13 @@
+package szkeleton;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Szkeleton sz = new Szkeleton();
+		
+		sz.runSzkeleton();
+
+	}
+
+}
--- szkeleton/src/szkeleton/Szkeleton.java
+++ szkeleton/src/szkeleton/Szkeleton.java
@@ -1,21 +1,80 @@
 package szkeleton;
 
+import java.util.Scanner;
+
 public class Szkeleton {
+	
+	private int numberOfScenarios = 40; //pl.
+	private boolean szkeletonIsRunning = true;
+	
+	public Szkeleton() {}
 
-	public static void main(String[] args) {
+	public void runSzkeleton() {
 
-		// TODO Auto-generated method stub
-		//KA
+		this.printSequenceOptions();
+		
+		while(szkeletonIsRunning) {
+			int sequenceNumber = readSequenceNumberFromUser();
+			if(sequenceNumber == 0) {
+				szkeletonIsRunning = false;
+			}
+			System.out.println("Szekvenciaszam beolvasva: " + sequenceNumber + "\n");
+		}
 
-
-		//Zsolt merge conflict pls
-
-
-		// Botond
-		//Zsolt
-		//Csinszka
-		//Márk
-
+	}
+	
+	private void printSequenceOptions() {
+		System.out.println("0 a kilepeshez\n"
+				+ "Settler move scenario -- 1\n"
+				+ "Robot move scenario -- 2\n"
+				+ "stb -- 3\n");
+	}
+	
+	private int readSequenceNumberFromUser() {
+		boolean validNumberread = false;
+		int number = -1;
+		
+		// Using Scanner for Getting Input from User 
+		Scanner in = new Scanner(System.in);
+		
+		while(!validNumberread) {
+			boolean numberRead = false;
+	        String s = in.nextLine(); 
+	        
+	        try {
+	            number = Integer.parseInt(s);
+	            numberRead = true;
+	        } catch (NumberFormatException nfe) {
+	        	numberRead = false;
+	            System.out.println("Kerlek egy szamot adj meg!\n");
+	        }
+	        
+	        if(numberRead) {
+	        	if((number <= numberOfScenarios) && (number >= 0)) {
+	        		validNumberread = true;
+	        	}
+	        	else {
+	        		validNumberread = false;
+	        		System.out.println("Kerlek egy szekvenciahoz tartozo szamot adj meg!\n");
+	        	}
+	        }
+		}
+		
+		
+		return number;
+	}
+	
+	
+	private void scenario1() {
+		
+	}
+	
+	private void scenario2() {
+		
+	}
+	
+	private void scenario3() {
+		
 	}
 
 }
