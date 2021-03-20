5d2815c0080c77f61adcd56997e4eafd99a69f1d
Csapo Botond <buzoganyharcos.csapo@gmail.com>
2021-03-20 09:33:34 +0100

    Map connect függvénye is kész Cs B

--- szkeleton/src/szkeleton/Map.java
+++ szkeleton/src/szkeleton/Map.java
@@ -13,7 +13,7 @@ public class Map implements Steppable{
         this.game = game;
     }
 
-    public void SolarStorm() {
+    public void SolarStorm() { // Az összes place-en lejátszódik az a szcenárió, amikor napvihar van
         for (int i = 0; i < places.length; i++) {
             places[i].HitByStorm();
         }
@@ -24,19 +24,24 @@ public class Map implements Steppable{
     public void EnpughResources() {
         game.Win();
     }
-    public void Step() {
+    public void Step() { // Az összes place-en meghívja a Step() függvényt
         for (int i = 0; i < places.length; i++) {
             places[i].Step();
         }
     }
-    public Place GetRandomPlace() {
+    public Place GetRandomPlace() { // Visszaad egy random place-t
         Random ran = new Random();
         return places[ran.nextInt(places.length)];
     }
 
     public void Connect() {
         for (int i= 0; i < places.length; i++) {
-
+            Random random = new Random();
+            int ran = random.nextInt(places.length);
+            if(!places[i].GetNeighbor(places[ran].placeID).equals(places[ran])  /*Ha még nem szomszédok, akkor...*/) {
+                places[i].AddNeighbor(places[ran]); //Beálítom egymás szomszédjának a két place-t
+                places[ran].AddNeighbor(places[i]);
+            }
         }
     }
 }
