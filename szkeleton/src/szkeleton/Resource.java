b2b53277bef6d2be6709f99ac6c65178a70aca74
CsinszkaVarga <v.csinszka@gmail.com>
2021-03-21 19:09:41 +0100

    Resources(és a többi) frissítve, Szkeletonban benne van a 4,13,14,23,24 esetek

--- szkeleton/src/szkeleton/Resource.java
+++ szkeleton/src/szkeleton/Resource.java
@@ -13,11 +13,6 @@ abstract public class Resource {
     public ArrayList<Integer> AddToList(ArrayList<Integer> I){
         return I;
     }
-
-    // MB: a removeFromList és az addToList paraméterezése ArrayList<Integer> kéne hogy legyen. Különben nem fogunk tudni
-    // dolgozni vele az aszteroidában meg a settlerben
-    // ja meg ide is kell szublimálásos függvény ami itt nem csinál semmit, csak az icewater-ben
-    // szublimálás
-    public void Sublimation(Asteroid a){}
+    public void Sublimation(Asteroid a){return;}
 
 }
